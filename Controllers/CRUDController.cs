bd39ee74de4396e94965e49dd1fe8bff139a33f5
Osirys <jlpimentel02@hotmail.com>
2023-06-03 10:04:07 -0400

    POST & GET METHOD ADDED

--- Controllers/CRUDController.cs
+++ Controllers/CRUDController.cs
@@ -30,6 +30,15 @@ public class CRUDController : ControllerBase
 
         return Ok(result);
     }
+    [HttpPost]
+    public async Task<IActionResult> Entrega([FromForm]Product product){
+        Ser_CRUD algo = new Ser_CRUD();
+        var result = algo.PostProduct(product);
+        await result;
+
+        return Ok(result);
+
+    }
 }
 
 
--- Services/Ser_CRUD.cs
+++ Services/Ser_CRUD.cs
@@ -14,14 +14,22 @@ public class Ser_CRUD
     }
     public Task<List<Product>> GetProducts()
     {
-       
         return _Context.Products.ToListAsync();
     }
-    public   ValueTask<Product> GetProduct(int Id){
-       var algo  =   _Context.Products.FindAsync(Id);
-
-        return algo ;
+    public ValueTask<Product> GetProduct(int Id)
+    {
+        var algo = _Context.Products.FindAsync(Id);
+        return algo;
     }
+    public Task<int> PostProduct(Product product)
+    {
+        _Context.Products.AddAsync(product);
+        var algo = _Context.SaveChangesAsync();
+        return algo;
+    }
+
+
+
 
 
 }
