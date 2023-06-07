7f2d143cbd808ada1620760344503502fa232bf0
Osirys <jlpimentel02@hotmail.com>
2023-06-07 12:06:57 -0400

    Cords Added

--- Program.cs
+++ Program.cs
@@ -1,15 +1,24 @@
 using NV.Models;
 using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<CountContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("Link")));
+builder.Services.AddDbContext<CountContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Link")));
+builder.Services.AddCors(O =>
+{
+    O.AddPolicy("Cords", option =>
+    {
+        option.
+        WithOrigins("*").
+        AllowAnyMethod().
+        AllowAnyHeader();
+    });
 
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,7 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors("Cords");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
